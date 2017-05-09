6b1fc661337bb6dcd19997618ca9e1bd667c2090
Martin Stabe <martinstabe@gmail.com>
2017-05-09 14:51:55 +0100

    Remove master image and add china story as demo

--- config/article.js
+++ config/article.js
@@ -23,15 +23,15 @@ export default () => ({ // eslint-disable-line
   },
 
   relatedArticle: {
-    text: 'Related article »',
-    url: 'https://en.wikipedia.org/wiki/Politics_and_the_English_Language',
+    text: '',
+    url: '',
   },
 
   mainImage: {
     title: '',
     description: '',
     credit: '',
-    url: 'https://image.webservices.ft.com/v1/images/raw/http%3A%2F%2Fcom.ft.imagepublish.prod.s3.amazonaws.com%2Fc4bf0be4-7c15-11e4-a7b8-00144feabdc0?source=ig&fit=scale-down&width=700',
+    url: '',
     width: 2048, // ensure correct width
     height: 1152, // ensure correct height
   },
