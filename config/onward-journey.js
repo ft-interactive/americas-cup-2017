13611bfe3f571f58ac117686c46524d070bb4b24
Martin Stabe <martinstabe@gmail.com>
2017-05-19 18:42:06 +0100

    delete all the things

--- config/onward-journey.js
+++
@@ -1,6 +0,0 @@
-export default () => ({
-  relatedContent: [
-    { rows: 1, list: 'thing/MDcxOWU1MzMtYTUzOC00NTA3LTk2ZDAtOWE1OWMwNGZlMGY3-VG9waWNz' },
-    { rows: 1, list: 'thing/MTcw-U2VjdGlvbnM=' },
-  ],
-});
