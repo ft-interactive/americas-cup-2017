b803254da999942ac3a17167a7c7cc58360f95a9
Martin Stabe <martinstabe@gmail.com>
2017-05-19 19:09:41 +0100

    fix OJ

--- config/onward-journey.js
+++ config/onward-journey.js
@@ -1,6 +1,6 @@
 export default () => ({
   relatedContent: [
-    { rows: 1, list: 'list/graphics' },
+    { rows: 1, list: 'thing/MDcxOWU1MzMtYTUzOC00NTA3LTk2ZDAtOWE1OWMwNGZlMGY3-VG9waWNz' },
     { rows: 1, list: 'thing/MTcw-U2VjdGlvbnM=' },
   ],
 });
